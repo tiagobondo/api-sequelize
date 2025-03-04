693b660c3b1db1e4ff0e3b024fbdd2137f703acc
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2025-03-04 17:03:35 +0100

    cors habilitado no projeto

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+
 import indexRouter from './routes/index.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 //Routes
 app.use('/', indexRouter)
